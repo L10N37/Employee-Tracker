c18869e6d00a6033b9db23673e8863971c1e3d0f
Lionel Sanderson <lfsanderson@hotmail.comgit config --global user.name Lionel>
2023-05-13 20:17:26 +1000

    functions finished

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,7 +7,7 @@ INSERT INTO role (title, salary, departmentId)
 VALUES ('Software Engineer', 120000, 1), ('Project Manager', 140000, 1), ('Sales Representative', 70000, 1), ('Human Resources Coordinator', 67000, 2), ('Marketing Manager', 72000, 2), ('IT Administrator', 90000, 2);
 
 INSERT INTO employee (firstName, lastName, roleId)
-VALUES ('Albert', 'Einwinkle', 1), ('Chiant', 'Beest', 4), ('Crah-Bee', 'Patty', 5);
+VALUES ('Albert', 'Einstein', 1), ('Fred', 'Durst', 4), ('Jasmine', 'Ryce', 5);
 
 INSERT INTO employee (firstName, lastName, roleId, managerId)
-VALUES ('Sarah', 'Connor', 2, 2), ('John', 'Doe', 3, 1), ('Elroy', 'McNoggin', 6, 3);
+VALUES ('Sarah', 'Connor', 2, 2), ('Chad', 'Grey', 3, 1), ('Mario', 'Duplantier', 6, 3);
--- index.js
+++ index.js
@@ -86,6 +86,7 @@ function showAsciiArt(value) {
 }
 
 // Show the main menu options
+// Show the main menu options
 function showMenu() {
   inquirer
     .prompt([
@@ -107,26 +108,26 @@ function showMenu() {
     ])
     .then((answers) => {
       switch (answers.option) {
-        case 'View all departments':
-          viewDepartments();
+        case 'View all departments':      
+          viewDepartments();                // [x]  resembles demonstration video
           break;
-        case 'View all roles':
-          viewRoles();
+        case 'View all roles':              
+          viewRoles();                      // []  resembles demonstration video
           break;
-        case 'View all employees':
-          viewEmployees();
+        case 'View all employees':          
+          viewEmployees();                  // [x]  resembles demonstration video
+          break;      
+        case 'Add a department':            
+          addDepartment();                  // [x]  resembles demonstration video
           break;
-        case 'Add a department':
-          addDepartment();
+        case 'Add a role':                  
+          addRole();                        // []  resembles demonstration video
           break;
-        case 'Add a role':
-          addRole();
+        case 'Add an employee':             
+          addEmployee();                    // [x]  resembles demonstration video
           break;
-        case 'Add an employee':
-          addEmployee();
-          break;
-        case 'Update an employee role':
-          updateEmployeeRole();
+        case 'Update an employee role':     
+          updateEmployeeRole();             // [x]  resembles demonstration video
           break;
         case 'Exit':
           connection.end();
@@ -141,62 +142,275 @@ function showMenu() {
 
 // Functions: These are using the promisify to ensure that the function executions are paused until the query resuls are available. This also allows us to 'catch' errors.
 
-// Function to view all departments
+//  function to view departments
 async function viewDepartments() {
+  showAsciiArt('Viewing Departments');
   try {
-    showAsciiArt('Viewing Departments');
     // Query the database to retrieve all departments
-    const results = await queryAsync('SELECT * FROM department');
+    const results = await connection.promise().query('SELECT * FROM department');
 
-    console.log('ID | Name');
-    results.forEach((department) => {
-    console.log(`${department.id} | ${department.name}`);
-    console.log('-----------------');
-    });
+    // Format the department data for table display
+    const departments = results[0].map((department) => ({
+      ID: department.id,
+      Name: department.name,
+    }));
+
+    // Display the departments in a table format
+    console.table(departments);
 
     // Return to the main menu
     showMenu();
   } catch (err) {
-    throw err;
+    console.error(err);
   }
 }
 
-// Function to view all roles
+
+//  function to view roles
 async function viewRoles() {
-  showAsciiArt('Viewing Roles');
   try {
-    // Query the database to retrieve all roles
-    const results = await queryAsync('SELECT * FROM role');
+    showAsciiArt('Viewing Job Roles');
+    
+    // Fetch all roles from the database
+    const roles = await connection.promise().query('SELECT * FROM role');
 
-    console.log('ID | Title | Salary | Department ID');
-    console.log('------------------------------------');
-    results.forEach((role) => {
-      console.log(`${role.id} | ${role.title} | ${role.salary} | ${role.departmentId}`);
-    });
+    // Display the roles in a table format
+    console.table(roles[0]);
 
     // Return to the main menu
     showMenu();
   } catch (err) {
-    throw err;
+    console.error(err);
   }
 }
 
-// Function to view all employees
+
+// function to view employees
 async function viewEmployees() {
   try {
-    showAsciiArt('Viewing Employees')
-    // Query the database to retrieve all employees
-    const results = await queryAsync('SELECT * FROM employee');
+    showAsciiArt('Viewing Employees');
 
-    console.log('ID | First Name | Last Name | Role ID | Manager ID');
-    console.log('---------------------------------------------------');
-    results.forEach((employee) => {
-      console.log(`${employee.id} | ${employee.firstName} | ${employee.lastName} | ${employee.roleId} | ${employee.managerId}`);
-    });
+    // Query the database to retrieve all employees with additional information
+    const query = `
+      SELECT
+        employee.firstName,
+        employee.lastName,
+        role.title AS jobRole,
+        department.name AS departmentName,
+        role.salary,
+        CONCAT(manager.firstName, ' ', manager.lastName) AS manager
+      FROM
+        employee
+      INNER JOIN role ON employee.roleId = role.id
+      INNER JOIN department ON role.departmentId = department.id
+      LEFT JOIN employee manager ON employee.managerId = manager.id
+    `;
+    const results = await queryAsync(query);
+
+    // Display the employees in a table format
+    console.table(results);
+
+    // Return to the main menu
+    showMenu();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// function to add deparment/s
+async function addDepartment() {
+  try {
+    showAsciiArt('Adding New Department');
+    const answers = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'departmentName',
+        message: 'Enter the name of the department:',
+        validate: (input) => {
+          if (input.trim() === '') {
+            return 'Please enter a department name.';
+          }
+          return true;
+        },
+      },
+    ]);
+
+    const departmentName = answers.departmentName;
+
+    // Insert the new department into the database
+    await connection.promise().query('INSERT INTO department SET ?', { name: departmentName });
+
+    console.log(`Department '${departmentName}' added successfully.`);
+
+    // Return to the main menu
+    showMenu();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// function to add roles
+  async function addRole() {
+    try {
+      showAsciiArt('Adding Job Role');
+  
+      // Fetch the list of departments
+      const departments = await connection.promise().query('SELECT * FROM department');
+      const departmentChoices = departments[0].map((department) => ({
+        name: department.name,
+        value: department.id,
+      }));
+  
+      const answers = await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'roleTitle',
+          message: 'Enter the title of the role:',
+          validate: (input) => {
+            if (input.trim() === '') {
+              return 'Please enter a role title.';
+            }
+            return true;
+          },
+        },
+        {
+          type: 'number',
+          name: 'roleSalary',
+          message: 'Enter the salary for the role:',
+          validate: (input) => {
+            if (isNaN(input) || input <= 0) {
+              return 'Please enter a valid salary.';
+            }
+            return true;
+          },
+        },
+        {
+          type: 'list',
+          name: 'departmentId',
+          message: 'Select the department for the role:',
+          choices: departmentChoices,
+        },
+      ]);
+  
+      const roleTitle = answers.roleTitle;
+      const roleSalary = answers.roleSalary;
+      const departmentId = answers.departmentId;
+  
+      // Insert the new role into the database
+      await connection.promise().query('INSERT INTO role SET ?', {
+        title: roleTitle,
+        salary: roleSalary,
+        departmentId: departmentId,
+      });
+  
+      console.log(`Role '${roleTitle}' added successfully.`);
+  
+      // Return to the main menu
+      showMenu();
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  
+// function to add employee/s
+async function addEmployee() {
+  try {
+    showAsciiArt('Adding Employee');
+
+    // Prompt the user for employee details
+    const employeeData = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'firstName',
+        message: "Enter the employee's first name:",
+      },
+      {
+        type: 'input',
+        name: 'lastName',
+        message: "Enter the employee's last name:",
+      },
+      {
+        type: 'list',
+        name: 'roleId',
+        message: "Select the employee's role:",
+        choices: async () => {
+          const roles = await queryAsync('SELECT id, title FROM role');
+          return roles.map(role => ({
+            name: role.title,
+            value: role.id
+          }));
+        }
+      },
+      {
+        type: 'list',
+        name: 'managerId',
+        message: "Select the employee's manager:",
+        choices: async () => {
+          const employees = await queryAsync('SELECT id, CONCAT(firstName, " ", lastName) AS name FROM employee');
+          return employees.map(employee => ({
+            name: employee.name,
+            value: employee.id
+          }));
+        }
+      }
+    ]);
+
+    // Insert the employee data into the database
+    await queryAsync('INSERT INTO employee SET ?', employeeData);
+    console.log('Employee added successfully.');
 
     // Return to the main menu
     showMenu();
   } catch (err) {
     throw err;
+  }
+}
+
+// function to update employee role
+async function updateEmployeeRole() {
+  try {
+    showAsciiArt('Updating Employee Role');
+
+    // Fetch the list of employees
+    const employees = await connection.promise().query('SELECT * FROM employee');
+    const employeeChoices = employees[0].map((employee) => ({
+      name: `${employee.firstName} ${employee.lastName}`,
+      value: employee.id,
+    }));
+
+    // Fetch the list of roles
+    const roles = await connection.promise().query('SELECT * FROM role');
+    const roleChoices = roles[0].map((role) => ({
+      name: role.title,
+      value: role.id,
+    }));
+
+    const answers = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'employeeId',
+        message: 'Select the employee to update:',
+        choices: employeeChoices,
+      },
+      {
+        type: 'list',
+        name: 'roleId',
+        message: 'Select the new role:',
+        choices: roleChoices,
+      },
+    ]);
+
+    const employeeId = answers.employeeId;
+    const roleId = answers.roleId;
+
+    // Update the employee's role in the database
+    await connection.promise().query('UPDATE employee SET roleId = ? WHERE id = ?', [roleId, employeeId]);
+
+    console.log(`Employee with ID '${employeeId}' role updated successfully.`);
+
+    // Return to the main menu
+    showMenu();
+  } catch (err) {
+    console.error(err);
   }
 }
