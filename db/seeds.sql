eba5f4d9c5b22d6f13cc4412e504d2d81c75e8d1
Lionel Sanderson <lfsanderson@hotmail.comgit config --global user.name Lionel>
2023-05-13 22:09:07 +1000

    more tidy up of seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,21 +1,22 @@
 USE employeedatabase;
 
 INSERT INTO department (name)
-VALUES
-  ('Engineering'),
-  ('Finance'),
-  ('Legal'),
-  ('Sales'),
-  ('HR');
+VALUES             -- ID
+  ('Engineering'), -- 1
+  ('Finance'),     -- 2
+  ('Legal'),       -- 3
+  ('Sales'),       -- 4
+  ('HR');          -- 5
 
 INSERT INTO role (title, salary, departmentId)
-VALUES
-  ('Software Engineer', 120000, 1),
-  ('Project Manager', 140000, 1),
-  ('Sales Representative', 70000, 4),
-  ('Human Resources Coordinator', 67000, 5),
-  ('Marketing Manager', 72000, 2),
-  ('IT Administrator', 90000, 1);
+VALUES                                               -- Role ID
+  ('Software Engineer', 120000, 1),                  -- 1
+  ('Project Manager', 140000, 5),                    -- 2
+  ('Sales Representative', 70000, 4),                -- 3
+  ('Receptionist', 49000, 5),                        -- 4
+  ('Human Resources Coordinator', 67000, 5),         -- 5
+  ('Marketing Manager', 72000, 2),                   -- 6
+  ('IT Administrator', 90000, 1);                    -- 7
 
 INSERT INTO employee (firstName, lastName, roleId, managerId)
 VALUES
@@ -24,4 +25,5 @@ VALUES
   ('Jasmine', 'Ryce', 5, NULL),
   ('Sarah', 'Connor', 2, 2),
   ('Chad', 'Grey', 3, 1),
-  ('Mario', 'Duplantier', 6, 3);
+  ('Mario', 'Duplantier', 6, 3),
+  ('James', 'Hetfield', 1, 1);
